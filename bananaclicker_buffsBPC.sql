808921257c2ba4277e5cbb2702b08c45bbc0a106
ghartemann <g.hartemann@gmail.com>
2022-05-23 20:05:54 +0200

    things

---
+++ bananaclicker_buffsBPC.sql
@@ -0,0 +1,3 @@
+insert into bananaclicker.buffsBPC (id, name, display_name, img, owned, price, multiplier, price_multiplier)
+values  (1, 'buffCursor', 'Meilleur curseur', 'cursor.png', 0, 100, 1, 1.31),
+        (2, 'buffMegaCursor', 'Curseur encore plus fort', 'cursor2.png', 0, 10000, 10, 1.21);
---
+++ bananaclicker_clickers.sql
@@ -0,0 +1,5 @@
+insert into bananaclicker.clickers (id, name, display_name, img, owned, price, multiplier, price_multiplier)
+values  (1, 'tierClicker', 'autoclicker', 'hand.png', 0, 50, 1, 1.16),
+        (2, 'tierTree', 'bananier', 'leaf.png', 0, 510, 11, 1.26),
+        (3, 'tierGorilla', 'gorille', 'gorilla.png', 0, 1420, 79, 1.3),
+        (4, 'tierMacaque', 'macaque', 'macaque.png', 0, 3800, 166, 1.4);
