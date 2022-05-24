1a591ee482c467a443cb6ab8cd0c4d78df7fdbfb
ghartemann <g.hartemann@gmail.com>
2022-05-24 21:45:51 +0200

    updated text

--- bananaclicker_buffsBPS.sql
+++
@@ -1,2 +0,0 @@
-insert into bananaclicker.buffsBPS (id, name, display_name, img, owned, price, multiplier, price_multiplier, modifies, modifies_name)
-values  (1, 'buffCPU', 'Robotiser les gorilles', 'cpu.png', 0, 3333, 5, 1.34, 'Gorilla', 'gorille');
--- database.sql
+++ database.sql
@@ -9,14 +9,15 @@ create table clickers
     owned            int         null,
     price            int         null,
     multiplier       float       null,
-    price_multiplier float       null
+    price_multiplier float       null,
+    `desc`           text        null
 );
 
-insert into clickers (id, name, display_name, img, owned, price, multiplier, price_multiplier)
-values  (1, 'tierClicker', 'autoclicker', 'hand.png', 0, 50, 1, 1.16),
-        (2, 'tierTree', 'bananier', 'leaf.png', 0, 510, 11, 1.26),
-        (3, 'tierGorilla', 'gorille', 'gorilla.png', 0, 1420, 79, 1.3),
-        (4, 'tierMacaque', 'macaque', 'macaque.png', 0, 3800, 166, 1.4);
+insert into bananaclicker.clickers (id, name, display_name, img, owned, price, multiplier, price_multiplier, desc)
+values  (1, 'tierClicker', 'autoclicker', 'hand.png', 0, 50, 1, 1.16, 'Un curseur qui clique tout seul. C''est pas formidable, ça ?'),
+        (2, 'tierTree', 'bananier', 'leaf.png', 0, 510, 11, 1.26, 'Un bananier tout ce qu''il y a de plus classique. Avec un sacré rendement tout de même.'),
+        (3, 'tierGorilla', 'gorille', 'gorilla.png', 0, 1420, 79, 1.3, 'Un gorille qui produit des bananes. Comment ? Pourquoi ? Ne me demandez pas.'),
+        (4, 'tierMacaque', 'macaque', 'macaque.png', 0, 3800, 166, 1.4, 'Un macaque qui fait à peu près la même chose que le gorille, mais beaucoup plus efficacement.');
 
 
 
